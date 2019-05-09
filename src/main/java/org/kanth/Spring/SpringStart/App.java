cde7c6aa52f36009f721ce870062ca621a6347d8
Kanth IAdmin <38177336+Kanthiadmin@users.noreply.github.com>
2019-05-09 16:49:02 +0530

    Debasis

--- src/main/java/org/kanth/Spring/SpringStart/App.java
+++ src/main/java/org/kanth/Spring/SpringStart/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Test Commit git ignore" );
+        System.out.println( "Test Commit git ignore dude" );
     }
 }
